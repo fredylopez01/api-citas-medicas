09f5b9dc177553542773ad99de564fb82c0110b2
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2025-09-02 22:43:35 -0500

    perf: Add dotenv and body-parser packages, and made some endpoints

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
-const PORT = 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send(`
+    <h1>Esta es mi API</h1>
+    <p>Esto es una api con nodejs y expressjs</p>
+    <p>Se esta ejecutando en el puerto ${PORT}</p>
+    `);
+});
+
+app.get("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  res.send(`Mostrar información del usuario con id: ${userId}`);
+});
+
+app.get("/search", (req, res) => {
+  const terms = req.query.termino || "No especificado";
+  const category = req.query.category || "Todas";
+  res.send(`
+    <h2>Resultado de busqueda</h2>
+    <p>Termino: ${terms}</p>
+    <p>Categoria: ${category}</p>
+    `);
 });
 
 app.listen(PORT, () => {

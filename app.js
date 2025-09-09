cde86d728f98644cc9c9ee62d68c6c23a0c45f26
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2025-09-09 00:24:16 -0500

    feat: Add error handler middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const loggerMiddleware = require("./middlewares/logger");
+const errorHandler = require("./middlewares/errorHandler");
 const { validateUser } = require("./utils/validation");
 
 const fs = require("fs");
@@ -13,6 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(loggerMiddleware);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
@@ -143,6 +145,10 @@ app.delete("/users/:id", (req, res) => {
       }
     });
   });
+});
+
+app.get("/error", (req, res, next) => {
+  next(new Error("Error intencional"));
 });
 
 app.listen(PORT, () => {
--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,13 +1,16 @@
-const loggerMiddleware = (req, res, next) => {
+const LoggerMiddleware = (req, res, next) => {
   const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] ${req.method} ${req.url} ${req.ip}`);
 
-  const start = Date().now();
+  console.log(`[${timestamp} ${req.method} ${req.url} - IP: ${req.ip}]`);
+
+  const start = Date.now();
+
   res.on("finish", () => {
     const duration = Date.now() - start;
     console.log(`[${timestamp}] Response: ${res.statusCode} - ${duration}ms`);
   });
+
   next();
 };
 
-module.exports = loggerMiddleware;
+module.exports = LoggerMiddleware;
